0ac070f310ec82085685e57bc8769f7d44c52423
TienDatGl <nguyentiendat.pleiku@gmail.com>
2023-09-28 16:33:51 +0700

    SignUP

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from "react-router-dom";
+
 
 function App() {
   return (
-   <h1>Hello World</h1>
+   <div>
+    <main>
+      <Outlet/>
+    </main>
+   </div>
   );
 }
 
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { SignUp } from './component/pages/SignUp';
+import { Login } from './component/pages/Login';
+import {Home} from './component/pages/Home'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path ='/' element={<App/>}>
+      <Route index element={<Home/>}/>
+      <Route path='signup' index element={<SignUp/>}/>
+      <Route path='login' index element={<Login/>}/>
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
