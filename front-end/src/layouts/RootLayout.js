03c94c108270aa6a8e5aa6622b7e9ad588898161
HaoLong4935 <longsachse@gmail.com>
2023-09-30 23:00:28 +0700

    Sprint1: NavBar Css tieu de

--- front-end/src/layouts/RootLayout.js
+++ front-end/src/layouts/RootLayout.js
@@ -5,33 +5,40 @@ const RootLayout = () => {
     return (
         <div className="root-layout">
             <header>
-                <nav className="bg-slate-400">
+                <nav className="bg-white p-5 border-b-2">
                     <div className="max-w-7xl mx-auto flex justify-between" >
                         {/* Ảnh Logo Trip Advisor */}
+                        <div>
+                            <img src="../assets/img/logo.png"></img>
+                        </div>
+
+                        {/* Các tiêu đề SeePost Discover About us */}
+                        <div className="hidden md:flex space-x-5 items-center font-semibold">
+                            <NavLink to='/' className="transition duration-200 ease-in-out bg-transparent hover:bg-slate-200 text-black py-2 px-4 rounded-full">Home</NavLink>
+                            <NavLink to='/Post' className="transition duration-200 ease-in-out bg-transparent hover:bg-slate-200 text-black py-2 px-4 rounded-full">See Post</NavLink>
+                            <NavLink to='/About-us' className="transition duration-200 ease-in-out bg-transparent hover:bg-slate-200 text-black py-2 px-4 rounded-full">About us</NavLink>
+                            <NavLink to='/Discover' className="transition duration-200 ease-in-out bg-transparent hover:bg-slate-200 text-black py-2 px-4 rounded-full">Discover</NavLink>
+                        </div>
+
+                        {/* Các Buttons */}
+                        <div className="hidden md:flex space-x-5 items-center">
+                            <button className="text-black font-bold text-lg">
+                                <NavLink to='login'>Login</NavLink>
+                            </button>
+                            <button className="bg-[#0f172a] hover:bg-[#2529d7] text-white font-bold py-2 px-4 border-b-4 border-[#000000] hover:border-[#10127e] rounded ">
+                                <NavLink to='signup'>Sign Up</NavLink>
+                            </button>
+                        </div>
+
+                        {/* ##### GIAO DIỆN MOBILE Ở ĐÂY ##### */}
                         <div>
 
                         </div>
 
-                        {/* Các tiêu đề SeePost Discover About us */}
-                        <div className="flex space-x-4 items-center">
-                            <NavLink to='/'>Home</NavLink>
-                            <NavLink to='/Post'>See Post</NavLink>
-                            <NavLink to='/About-us'>About us</NavLink>
-                            <NavLink to='/Discover'>Discover</NavLink>
-                        </div>
-
-                        {/* Các Buttons */}
-                        <div className="flex space-x-2 items-center">
-                            <button className="bg-[#0f172a] hover:bg-[#2529d7] text-white font-bold py-2 px-4 border-b-4 border-[#000000] hover:border-[#10127e] rounded ">
-                                <NavLink to='signup'>Sign Up</NavLink>
-                            </button>
-                            <button className="bg-[#0f172a] hover:bg-[#2529d7] text-white font-bold py-2 px-4 border-b-4 border-[#000000] hover:border-[#10127e] rounded">
-                                <NavLink to='login'>Login</NavLink>
-                            </button>
-                        </div>
                     </div>
                 </nav>
             </header>
+
             {/* Outlet sẽ hiển thị nội dung của route được kích hoạt trong ParentComponent. 
             Bất cứ khi nào bạn truy cập một đường dẫn được chỉ định trong Route, 
             Outlet sẽ hiển thị nội dung của route đó. */}
